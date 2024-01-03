68f0d36db5e8f7e31d423c015e7d56c1a5522689
rifqiabd <rqi.rifqi@gmail.com>
2024-01-04 06:54:18 +0700

    Penambahan 2 file belajar

---
+++ TipeDataNumber.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b111111);
+
+echo "Underscore di Number : ";
+var_dump(1_2332_2);
